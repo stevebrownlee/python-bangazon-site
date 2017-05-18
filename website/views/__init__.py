4902875ba4366cc78a43bb53ee9b1f756aa7b3a9
Adam Myers <ANMyers53@yahoo.com>
2017-05-18 10:42:55 -0500

    reformated file structure for handling multiple forms, models, views with respective directories per

---
+++ website/views/__init__.py
@@ -0,0 +1,9 @@
+"""
+ Import your views here :)
+ Note: please specify file path from root directory
+ 
+ example: 
+
+ from website.views.(your file here) import (class or method name here)
+"""
+from website.views.views import *

b8f04ebae8e55d6f403ebcf76b6f18eec626b23b
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-05-18 13:02:55 -0500

    Finished all models for this sprint

--- website/models.py
+++ website/models.py
@@ -2,12 +2,95 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    """
+    purpose: Creates Category table within database
+        Example useage: 
+
+    author: Taylor Perkins, Justin Short
+
+    args: models.Model: (NA): models class given by Django
+
+    returns: (None): N/A
+    """      
+    category_name = models.TextField(blank=True, null=False, max_length=50)    
+
 class Product(models.Model):
+    """
+    purpose: Creates Product table within database
+        Example useage: 
+
+    author: Taylor Perkins, Justin Short
+
+    args: models.Model: (NA): models class given by Django
+
+    returns: (None): N/A
+    """      
     seller = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
     )
+    product_category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.IntegerField(null=False)
+    description = models.TextField(blank=True, null=False, max_length=500)
+    price = models.DecimalField(null=False, decimal_places=2)
+    date_created = models.DateField(auto_now_add=True)  # This auto generates date on creation
+    title = models.CharField(max_length=255)
+
+class User(models.Model):
+
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     quantity = models.IntegerField()
+
+class PaymentType(models.Model):
+    """
+    purpose: Creates PaymentType table within database
+        Example useage: 
+
+    author: Taylor Perkins, Justin Short
+
+    args: models.Model: (NA): models class given by Django
+
+    returns: (None): N/A
+    """   
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )   
+    name = models.TextField(blank=True, null=False, max_length=50) 
+    account_number = models.IntegerField(range(12, 19))
+
+
+class Order(models.Model):
+    """
+    purpose: Creates Order table within database
+        Example useage: 
+
+    author: Taylor Perkins, Justin Short
+
+    args: models.Model: (NA): models class given by Django
+
+    returns: (None): N/A
+    """   
+    buyer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )   
+    date_complete = models.DateField(null=True, auto_now=False, auto_now_add=False)  # This will get filled upon order completion
+
+
+
+
+
+
+
+
+
+
+

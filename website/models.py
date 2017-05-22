09f687b3642ca510e7b03a5881834458756281fd
Maxwell C Baldridge <max.baldridge@gmail.com>
2017-05-22 14:12:14 -0500

    adds fixtures for products and a user

--- website/models.py
+++ website/models.py
@@ -2,6 +2,20 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(User):
+    """
+    purpose: Instantiates a customer, and pulls in Django's default user model
+    author: Max Baldridge
+    args: User: model class given by Django
+    returns: (None): N/A
+    """      
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):  # __unicode__ on Python 2
+        return "This user's name is {}".format(self.user.first_name)
+
+
+
 class ProductType(models.Model):
     product_type_name = models.CharField(max_length=255, null=True)
 

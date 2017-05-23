cfdb18ea13c6ae8e6fc1f5392538bf8efd4cbe37
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-05-23 15:09:15 -0500

    Updated models.py

--- website/models.py
+++ website/models.py
@@ -5,14 +5,14 @@ from django.db import models
 class Profile(User):
     """
     purpose: Creates Category table within database
-        Example useage:
+        Example useage: 
 
     author: Taylor Perkins, Justin Short
 
     args: models.Model: (NA): models class given by Django
 
     returns: (None): N/A
-    """
+    """      
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):  # __unicode__ on Python 2
@@ -22,70 +22,40 @@ class Profile(User):
 class Category(models.Model):
     """
     purpose: Creates Category table within database
-        Example useage:
+        Example useage: 
 
     author: Taylor Perkins, Justin Short
 
     args: models.Model: (NA): models class given by Django
 
     returns: (None): N/A
-    """
-    category_name = models.TextField()
+    """      
+    category_name = models.TextField()    
 
     def __str__(self):  # __unicode__ on Python 2
         return self.category_name
-
-class Product(models.Model):
-    """
-    purpose: Creates Product table within database
-        Example useage:
-
-    author: Taylor Perkins, Justin Short
-
-    args: models.Model: (NA): models class given by Django
-
-    returns: (None): N/A
-    """
-    seller = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    product_category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-    )
-    quantity = models.IntegerField(null=False)
-    description = models.TextField(null=False, max_length=500)
-    price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
-    date_created = models.DateField(auto_now=True, auto_now_add=False)  # This auto generates date on creation
-    title = models.CharField(max_length=255)
-
-    def __str__(self):  # __unicode__ on Python 2
-        return self.title
 
     def get_products(self):
         print(dir(self))
         return Product.objects.filter(product_category=self)
 
 
-
 class PaymentType(models.Model):
     """
     purpose: Creates PaymentType table within database
-        Example useage:
+        Example useage: 
 
-    author: Taylor Perkins, Justin Short, Harry Epstein
+    author: Taylor Perkins, Justin Short
 
     args: models.Model: (NA): models class given by Django
 
     returns: (None): N/A
-
-    """
+    """       
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-    )
-    name = models.TextField(null=False, max_length=50)
+    )   
+    name = models.TextField(blank=True, null=False, max_length=50) 
     account_number = models.IntegerField(range(12, 20))
 
     def __str__(self):  # __unicode__ on Python 2
@@ -95,40 +65,39 @@ class PaymentType(models.Model):
 class Order(models.Model):
     """
     purpose: Creates Order table within database
-        Example useage:
+        Example useage: 
 
     author: Taylor Perkins, Justin Short
 
     args: models.Model: (NA): models class given by Django
 
     returns: (None): N/A
-
-    """
+    """       
     buyer = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-    )
+    )   
     payment_type = models.ForeignKey(
         PaymentType,
         on_delete=models.CASCADE,
         null=True,
         blank=True
-    )
+    )   
     date_complete = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)  # This will get filled upon order completion
 
 
 class Product(models.Model):
     """
     purpose: Creates Product table within database
-        Example useage:
+        Example useage: 
 
     author: Taylor Perkins, Justin Short, Casey Dailey
 
     args: models.Model: (NA): models class given by Django
 
     returns: (None): N/A
-    """
-    order = models.ManyToManyField(Order)
+    """    
+    order = models.ManyToManyField(Order)  
     seller = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -154,3 +123,5 @@ class Product(models.Model):
 
     def __str__(self):  # __unicode__ on Python 2
         return self.title
+
+

70e62997c7ae27a3b19c44e21b01b167f08e4f4b
Dean Smith <Dean Smith>
2017-05-23 23:11:00 -0500

    Starting tests

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
+# Verify that the Payment Types view for a customer has all of the payment types in the request context
+def test_list_payment_types:
+	
 
-def test_

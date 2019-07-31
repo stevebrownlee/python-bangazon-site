0722283eb1ba45a2607d17d5c74d1904d39ef9cd
Steve Brownlee <chortlehoort@gmail.com>
2019-07-30 22:51:43 -0500

    Updated for correct migration

--- website/models/__init__.py
+++ website/models/__init__.py
@@ -1,1 +1,6 @@
 from .product import Product
+from .product_type import ProductType
+from .customer import Customer
+from .order import Order
+from .order_product import OrderProduct
+from .paymenttype import PaymentType
--- website/views/index.py
+++ website/views/index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from website.models import Product
 
 def index(request):
+    all_products = Product.objects.all()[:20]
     template_name = 'index.html'
-    return render(request, template_name, {})
+    return render(request, template_name, {"products": all_products})
